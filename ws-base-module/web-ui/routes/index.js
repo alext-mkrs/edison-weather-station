c54eaf60fdec4923db247f94ebb47f96cc665431
Alex Tereschenko <alext.mkrs@gmail.com>
2016-03-27 14:57:34 +0000

    Split sensor interface into a separate module out of web UI

--- ws-base-module/web-ui/routes/index.js
+++ ws-base-module/web-ui/routes/index.js
@@ -1,52 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-var bleSensors = require('../../ws-ble-sensors.js');
-var localSensors = require('../../ws-local-sensors.js');
-
-// Utility function to get sensor data array from data acquisition module
-function getSensorData() {
-  var bleSensorDataAssoc = bleSensors.getBleSensorData();
-  var localSensorDataAssoc = localSensors.getLocalSensorData();
-  var sensorData = [];
-
-  // creating another array is ugly, but there seems to be no way of iterating over assoc array in Jade
-  // first, let's add data from BLE sensors
-  for (var sensorDataItem in bleSensorDataAssoc) {
-    if (bleSensorDataAssoc.hasOwnProperty(sensorDataItem)) {
-      sensorData.push(bleSensorDataAssoc[sensorDataItem]);
-    }
-  }
-
-  // next, the local ones
-  for (sensorDataItem in localSensorDataAssoc) {
-    if (localSensorDataAssoc.hasOwnProperty(sensorDataItem)) {
-      sensorData.push(localSensorDataAssoc[sensorDataItem]);
-    }
-  }
-
-  // let's sort this alphabetically using full module name string
-  // to avoid having a different list each time (that's an assoc array/object)
-  return sensorData.sort(function(a, b) {
-      if (a.moduleName > b.moduleName) {
-          return 1;
-      }
-      if (a.moduleName < b.moduleName) {
-          return -1;
-      }
-
-      return 0;
-  });
-}
+var wsSensors = require('../../ws-sensors.js');
 
 // index
 router.get('/', function(req, res, next) {
-  res.render('index', { sensorData: getSensorData() });
+  res.render('index', { sensorData: wsSensors.getSensorData() });
 });
 
 // our micro-API endpoint for AJAX
 router.get('/get_sensor_data', function(req, res, next) {
-  res.send({ data: getSensorData() });
+  res.send({ data: wsSensors.getSensorData() });
 });
 
 module.exports = router;
---
+++ ws-base-module/ws-sensors.js
@@ -0,0 +1,41 @@
+var WsSensors = function() {};
+
+// Utility function to get sensor data array from all data acquisition modules
+WsSensors.prototype.getSensorData = function() {
+  var bleSensors = require('./ws-ble-sensors.js');
+  var localSensors = require('./ws-local-sensors.js');
+
+  var bleSensorDataAssoc = bleSensors.getBleSensorData();
+  var localSensorDataAssoc = localSensors.getLocalSensorData();
+  var sensorData = [];
+
+  // creating another array is ugly, but there seems to be no way of iterating over assoc array in Jade
+  // first, let's add data from BLE sensors
+  for (var sensorDataItem in bleSensorDataAssoc) {
+    if (bleSensorDataAssoc.hasOwnProperty(sensorDataItem)) {
+      sensorData.push(bleSensorDataAssoc[sensorDataItem]);
+    }
+  }
+
+  // next, the local ones
+  for (sensorDataItem in localSensorDataAssoc) {
+    if (localSensorDataAssoc.hasOwnProperty(sensorDataItem)) {
+      sensorData.push(localSensorDataAssoc[sensorDataItem]);
+    }
+  }
+
+  // let's sort this alphabetically using full module name string
+  // to avoid having a different list each time (that's an assoc array/object)
+  return sensorData.sort(function(a, b) {
+      if (a.moduleName > b.moduleName) {
+          return 1;
+      }
+      if (a.moduleName < b.moduleName) {
+          return -1;
+      }
+
+      return 0;
+  });
+};
+
+module.exports = new WsSensors();
