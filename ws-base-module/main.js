d0e20b5b095bf99f03c93a4bd4042e8f8fa9bf1f
Alex Tereschenko <alext.mkrs@gmail.com>
2016-05-08 19:08:05 +0000

    main.js: corrected log output styling

--- ws-base-module/main.js
+++ ws-base-module/main.js
@@ -1,7 +1,7 @@
 var webui = require("./web-ui/bin/www");
 var cloud = require("./ws-cloud-upload.js");
 
-console.log("Starting main app");
+console.log("Starting main app for Edison Weather Station");
 
 process.on('SIGINT', function() {
     console.log("Caught interrupt signal, exiting");
